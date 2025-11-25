bc2ee25ae88621d034053c9d986602a8adf951a1
iyiola olayinka <starheaddeji@gmail.com>
2025-11-25 11:47:02 +0100

    dashboard setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
+import Dashboard from './components/Dashboard'
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route index path="/" element={<Login />}></Route>
+        <Route path ="Dashboard" element ={<Dashboard />}></Route>
       </Routes>
     </>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      Login
+      <h1 className='text-red-500'>Login</h1>
     </div>
   )
 }
